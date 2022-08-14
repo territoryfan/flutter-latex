11ddccd2bc6fbbe5565101e40a1c115f69c0b5da
zairan.fan <territoryfan@163.com>
2022-08-14 14:42:03 +0800

    first commit1

---
+++ android/app/src/main/kotlin/com/example/katex_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.katex_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tex/flutter_tex.dart';
+import 'package:katex_demo/tex_view_document_example.dart';
+import 'package:katex_demo/tex_view_quiz_example.dart';
+
+main() {
+  runApp(const FlutterTeXExample());
+}
+
+class FlutterTeXExample extends StatelessWidget {
+  const FlutterTeXExample({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TeXViewFullExample(),
+    );
+  }
+}
+
+class TeXViewFullExample extends StatefulWidget {
+  const TeXViewFullExample({Key? key}) : super(key: key);
+
+  @override
+  _TeXViewFullExampleState createState() => _TeXViewFullExampleState();
+}
+
+class _TeXViewFullExampleState extends State<TeXViewFullExample> {
+  int radVal = 0;
+
+  late TeXViewRenderingEngine renderingEngine;
+
+  @override
+  Widget build(BuildContext context) {
+    renderingEngine = radVal == 0
+        ? const TeXViewRenderingEngine.katex()
+        : const TeXViewRenderingEngine.mathjax();
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text("Flutter TeX (Demo)"),
+      ),
+      body: ListView(
+        shrinkWrap: true,
+        children: <Widget>[
+          RadioListTile<int>(
+            value: 0,
+            groupValue: radVal,
+            onChanged: (val) {
+              setState(() {
+                radVal = val!;
+              });
+            },
+            title: const Text("Katex"),
+            subtitle: const Text("RenderingEngine for Fast Rendering"),
+          ),
+          RadioListTile<int>(
+            value: 1,
+            groupValue: radVal,
+            onChanged: (val) {
+              setState(() {
+                radVal = val!;
+              });
+            },
+            title: const Text("MathJax"),
+            subtitle: const Text("RenderingEngine for Quality Rendering"),
+          ),
+          const Divider(
+            height: 30,
+            color: Colors.transparent,
+          ),
+          getExampleButton(
+              context,
+              'Quiz Example',
+              TeXViewQuizExample(
+                renderingEngine: renderingEngine,
+              )),
+          getExampleButton(
+              context,
+              'TeX Examples',
+              TeXViewDocumentExamples(
+                renderingEngine: renderingEngine,
+              )),
+        ],
+      ),
+    );
+  }
+
+  getExampleButton(BuildContext context, String title, Widget widget) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: ElevatedButton(
+        style: ButtonStyle(
+            elevation: MaterialStateProperty.all(5),
+            backgroundColor: MaterialStateProperty.all(Colors.white)),
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => widget));
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Text(
+            title,
+            style: const TextStyle(fontSize: 15, color: Colors.black),
+          ),
+        ),
+      ),
+    );
+  }
+}
